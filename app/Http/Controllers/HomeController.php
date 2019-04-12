9c9c603f8f7b20da2350deee793d04b26d05c506
Peterng091289 <1512230050@ftu.edu.vn>
2019-04-12 14:22:21 +0700

    fix function storeUser -hash

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Food;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 
 class HomeController extends Controller
@@ -68,7 +69,7 @@ class HomeController extends Controller
         $user = new User();
         $user->name = $request->input('name');
         $user->email = $request->input('email');
-        $user->password = $request->input('password');
+        $user->password = Hash::make($request->input('password')); //them hash
         $user->save();
         return redirect()->route('admin.users.list');
     }
