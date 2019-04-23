b1893d0d8c94cc2121f077bbb412e62d964f0402
hoadao <daoduchoak60cc@gmail.com>
2019-04-23 13:49:10 +0700

    m

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Advertisement;
+use App\Food;
 
 class HomeController extends Controller
 {
     public function getAllAdvertisement() {
         $advertisements = Advertisement::all();
         return view('/welcome', compact('advertisements'));
+    }
+    public function getAllFood() {
+        $foods = Food::all();
+        return view('food-page.ListFood', compact('foods'));
     }
 }
