2eba8f83184a1c2f5350ab360a980d84c599e3f6
Peterng091289 <1512230050@ftu.edu.vn>
2019-04-24 17:02:41 +0700

    change bg order

--- resources/views/home-page/order-table/index2.blade.php
+++ resources/views/home-page/order-table/index2.blade.php
@@ -1,24 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="keywords"
-          content="Design Order Form Widget Responsive, Login form web template,Flat Pricing tables,Flat Drop downs  Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design"/>
-    <script
-        type="application/x-javascript"> addEventListener('load', function () { setTimeout(hideURLbar, 0) }, false)
-
-        function hideURLbar () { window.scrollTo(0, 1) } </script>
-    <!-- font files -->
-    <link
-        href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic'
-        rel='stylesheet' type='text/css'>
-    <!-- /font files -->
-    <!-- css files -->
-    <link href="{{'css/style.css'}}" rel='stylesheet' type='text/css' media="all"/>
-    <!-- /css files -->
-</head>
-<body>
+{{--@extends('home-page.header')--}}
+<link href="{{asset('css/order-style.css')}}" rel='stylesheet' type='text/css' media="all"/>
 <h1 class="header-w3ls">Order Form</h1>
 <div class="content-w3ls">
     <div class="form-w3ls">
@@ -127,5 +108,4 @@
         </form>
     </div>
 </div>
-</body>
-</html>
+@include('home-page.footer')
