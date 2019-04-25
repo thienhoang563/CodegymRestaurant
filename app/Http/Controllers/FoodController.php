1c639a34fd718b7e58d4e6eaa97f66b01f040004
truonghoatm <truonghoahd123@gmail.com>
2019-04-25 09:11:52 +0700

    fix upload image error in admin-page

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -25,17 +25,12 @@ class FoodController extends Controller
         $food->food_description = $request->input('food_description');
         $food->food_price = $request->input('food_price');
         $food->food_rating = $request->input('food_rating');
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
+        if ($request->hasFile('food_image')) {
+            $image = $request->file('food_image');
             $path = $image->store('images', 'public');
             $food->food_image = $path;
         }
-//        else{
-//            $fileName = $file;
-//            $newFileName = $fileName;
-//            $request->file('inputFile')->storeAs('public/image', $newFileName);
-//            $food->food_image = $newFileName;
-//        }
+
         $food->save();
         Session::flash('success', 'Đã thêm món ăn mới.');
         return redirect()->route('admin.foods.list');
@@ -55,13 +50,10 @@ class FoodController extends Controller
         $food->food_price = $request->input('food_price');
         $food->food_rating = $request->input('food_rating');
         $file = $request->inputFile;
-        if (!$request->hasFile('inputFile')) {
-            $food->food_image = $file;
-        } else {
-            $fileName = $file->getClientOriginalName();
-            $newFileName = $fileName;
-            $request->file('inputFile')->storeAs('public/image', $newFileName);
-            $food->food_image = $newFileName;
+        if ($request->hasFile('food_image')) {
+            $image = $request->file('food_image');
+            $path = $image->store('images', 'public');
+            $food->food_image = $path;
         }
         $food->save();
         Session::flash('success', 'Đã sửa món ăn.');
